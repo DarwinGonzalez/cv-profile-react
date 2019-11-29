a34e0fb5a394de736b333a202f3b2e50bbcb6316
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-11-29 10:41:51 +0100

    Setting up github pages

---
+++ build/precache-manifest.3eb1dcea0755d115d34517719541a390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f82282b5aedf809220676731985179",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ac45d02ed0a28f6443e",
+    "url": "/static/css/2.1b0c6b0a.chunk.css"
+  },
+  {
+    "revision": "d3eedc564934e12e7c59",
+    "url": "/static/css/main.669671ee.chunk.css"
+  },
+  {
+    "revision": "7ac45d02ed0a28f6443e",
+    "url": "/static/js/2.09d14c8a.chunk.js"
+  },
+  {
+    "revision": "d3eedc564934e12e7c59",
+    "url": "/static/js/main.6aa33dfa.chunk.js"
+  },
+  {
+    "revision": "6021f8915c70ca1ce83c",
+    "url": "/static/js/runtime-main.acd730f6.js"
+  }
+]);
