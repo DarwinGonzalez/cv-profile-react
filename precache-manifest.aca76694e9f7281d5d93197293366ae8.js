06173a9f95a49b3d24c2269bf3c71902a420f0ee
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-11-29 17:21:57 +0100

    Updates

--- precache-manifest.aca76694e9f7281d5d93197293366ae8.js
+++ precache-manifest.aca76694e9f7281d5d93197293366ae8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bf6d38e5aeb6e7e4450996bffecabdac",
+    "revision": "1e9ee7cbcc6e517ea47c8b72be02c397",
     "url": "/cv-profile-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv-profile-react/static/css/2.1b0c6b0a.chunk.css"
   },
   {
-    "revision": "7b78720388cf253d8d3c",
+    "revision": "7564c37030f15294e148",
     "url": "/cv-profile-react/static/css/main.669671ee.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/cv-profile-react/static/js/2.6b5ffbc2.chunk.js"
   },
   {
-    "revision": "7b78720388cf253d8d3c",
-    "url": "/cv-profile-react/static/js/main.62e8ba01.chunk.js"
+    "revision": "7564c37030f15294e148",
+    "url": "/cv-profile-react/static/js/main.ed87f892.chunk.js"
   },
   {
     "revision": "b522362673ea99a8573f",
