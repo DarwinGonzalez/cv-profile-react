c85668b751b1ef791729e6ff80fa2452ba024315
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-11-29 10:49:20 +0100

    Updates

---
+++ precache-manifest.339c6529c62f78987828db13d4f897f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512a4dc2e8c3aab37ec624ab8643b698",
+    "url": "/cv-profile-react/index.html"
+  },
+  {
+    "revision": "2ab56f2e1094438dc088",
+    "url": "/cv-profile-react/static/css/2.1b0c6b0a.chunk.css"
+  },
+  {
+    "revision": "7b78720388cf253d8d3c",
+    "url": "/cv-profile-react/static/css/main.669671ee.chunk.css"
+  },
+  {
+    "revision": "2ab56f2e1094438dc088",
+    "url": "/cv-profile-react/static/js/2.6b5ffbc2.chunk.js"
+  },
+  {
+    "revision": "7b78720388cf253d8d3c",
+    "url": "/cv-profile-react/static/js/main.62e8ba01.chunk.js"
+  },
+  {
+    "revision": "b522362673ea99a8573f",
+    "url": "/cv-profile-react/static/js/runtime-main.e0d4dd4f.js"
+  }
+]);
